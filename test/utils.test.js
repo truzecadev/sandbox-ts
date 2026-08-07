cffa30b1b65c816ad72d5290c05cd071f5b7244e
truzecadev <vwttaqj866@hotmail.com>
2026-08-07 05:00:01 +0100

    test: cover chunk

--- test/utils.test.js
+++ test/utils.test.js
@@ -6,3 +6,5 @@ test('unique is defined', () => { expect(u.unique).toBeDefined(); });
 
 test('clamp is defined', () => { expect(u.clamp).toBeDefined(); });
 
+test('chunk is defined', () => { expect(u.chunk).toBeDefined(); });
+
